061221883a6cecac8bb092ac270ca61d27c8dbe8
Simile <220214440+simile-or-not@users.noreply.github.com>
2025-12-03 20:58:44 +0300

    so

--- start-bot.sh
+++ start-bot.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+mkdir -p config
+touch config/token.txt
+touch config/db.json
+touch config/exceptions.txt
+touch config/quotes.txt
+touch config/swearing.txt
+touch config/users.txt
+
 ./python-restarter.sh bot/main.py
